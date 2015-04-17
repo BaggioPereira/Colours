7742941d47aad56d8394def823e080ca05ef36c8
Baggio Pereira <LemonsJoke@users.noreply.github.com>
2015-04-17 22:15:05 +0100

    Colour swaps work
    Testing colour swaps

--- Assets/Scripts/Swap.cs
+++ Assets/Scripts/Swap.cs
@@ -2,14 +2,34 @@
 using System.Collections;
 
 public class Swap : MonoBehaviour {
+    Material red, green, blue;
+    GameObject plane;
+    Renderer rend;
 
 	// Use this for initialization
 	void Start () {
-	
+        plane = GameObject.Find("Plane");
+        red = (Material) Resources.Load("ShadersMaterials/Red");
+        green = (Material) Resources.Load("ShadersMaterials/Green");
+        blue = (Material) Resources.Load("ShadersMaterials/Blue");
+        rend = GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rend.material = blue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rend.material = green;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rend.material = red;
+        }
 	}
 }
