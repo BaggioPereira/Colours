91fbd17072b966a6d7d84aa6f4594662ccd2f0ec
Baggio Pereira <LemonsJoke@users.noreply.github.com>
2015-04-27 12:39:34 +0100

    Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,28 +3,22 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
     GameObject player;
-     public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-     public RotationAxes axes = RotationAxes.MouseXAndY;
-     public float sensitivityX = 15F;
-     public float sensitivityY = 15F;
-
-     public float minimumX = -360F;
-     public float maximumX = 360F;
- 
-     public float minimumY = -60F;
-    public float maximumY = 60F;
- 
-     float rotationX = 0F;
-     float rotationY = 0F;
- 
-    Quaternion originalRotation;
+    CharacterController controller;
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15f;
+    public float sensitivityY = 15f;
+    public float minimumY = -60f;
+    public float maximumY = 60f;
+    float rotationY = 0f;
+    private Vector3 moveDirection = Vector3.zero;
 
 	// Use this for initialization
 	void Start () 
     {
-        player = GameObject.Find("Player");
-        if (GetComponent<Rigidbody>())
-            GetComponent<Rigidbody>().freezeRotation = true;
+        player = GameObject.FindGameObjectWithTag("Player");
+        player.AddComponent<CharacterController>();
+        controller = (CharacterController)player.transform.GetComponent("CharacterController");
 	}
 	
 	// Update is called once per frame
@@ -33,24 +27,25 @@ public class Player : MonoBehaviour {
         if (axes == RotationAxes.MouseXAndY)
         {
             float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-         
             rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-         
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
             transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
         }
-
         else if (axes == RotationAxes.MouseX)
         {
             transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
         }
-
         else
         {
             rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-         
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
             transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
         }
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= 20.0f;
+        moveDirection.y = 0;
+        controller.Move(moveDirection * Time.deltaTime);
 	}
 }
