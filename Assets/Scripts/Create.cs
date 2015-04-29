b5b173d58bb6846a3b8cadf885da8d55093100bd
Baggio Pereira <LemonsJoke@users.noreply.github.com>
2015-04-29 11:07:25 +0100

    Simple activate and deactivate of item

---
+++ Assets/Scripts/Create.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Create : MonoBehaviour {
+    public GameObject first, needactive;
+    public bool reappear;
+	// Use this for initialization
+	void Start () 
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (first.GetComponent<ButtonPress>().pressed)
+        {
+            reappear = true;
+        }
+
+        else
+        {
+            reappear = false;
+        }
+
+        if (reappear)
+        {
+            needactive.SetActive(true);
+        }
+
+        else
+        {
+            needactive.SetActive(false);
+        }
+	}
+}
