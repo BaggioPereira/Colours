839b0ca73a9896cc4a1ce3f61d865c2ebbad7f15
Baggio Pereira <LemonsJoke@users.noreply.github.com>
2015-05-03 11:50:19 +0100

    Testing Multi Dimensional Array
    For future implementation

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+    public TwoDimensionalArray[] Array;
+	// Use this for initialization
+	void Start () 
+    {
+        for (int i = 0; i < Array.Length; i++ )
+        {
+            Array[i].active = new bool[Array[i].switchs.Length];
+        }
+            //for (int i = 0; i < Array.Length; i++)
+            //{
+            //    //Debug.Log(i.ToString());
+            //    for (int j = 0; j < Array[i].objects.Length; j++)
+            //    {
+            //        //Debug.Log(Array[i].objects[j].name);
+            //    }
+            //}
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        for(int i  = 0; i < Array.Length; i++)
+        {
+            for(int j = 0; j<Array[i].switchs.Length; j++)
+            {
+                if(Array[i].switchs[j] == null)
+                {
+                    Array[i].active[j] = false;
+                }
+
+                else
+                {
+                    Array[i].active[j] = true;
+                }
+            }
+        }
+	}
+}
